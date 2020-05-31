b691905ae9eedb6e2c7d8951d23dcfdf1c5b09e4
v_hjxu <v_hjxu@digitalgd.com.cn>
2020-05-31 09:45:25 +0800

    [init]初始化项目

---
+++ demo1/src/main/java/com/aco/practice/demo1/Demo1Application.java
@@ -0,0 +1,15 @@
+package com.aco.practice.demo1;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.aco.practice.demo1")
+public class Demo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo1Application.class, args);
+    }
+
+}
