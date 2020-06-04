6f692a019beeb8853025eea35dfeb13612d686f6
v_hjxu <v_hjxu@digitalgd.com.cn>
2020-06-04 22:42:41 +0800

    [add]添加redis配置

--- demo1/src/main/java/com/aco/practice/demo1/controller/DemoController.java
+++ demo1/src/main/java/com/aco/practice/demo1/controller/DemoController.java
@@ -1,7 +1,11 @@
 package com.aco.practice.demo1.controller;
 
+import com.aco.practice.demo1.util.ApiResponse;
+import com.aco.practice.demo1.util.RedisKeyUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +19,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(value = "测试接口")
 public class DemoController {
 
-    @ApiOperation(value = "demo 测试接口",notes = "demo 测试接口")
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @ApiOperation(value = "demo 测试接口", notes = "demo 测试接口")
     @GetMapping(value = "demo")
     public void test(){
 
+    }
+
+    @ApiOperation(value = "测试redis接口", notes = "测试redis接口")
+    @GetMapping(value = "redis-test")
+    public ApiResponse setAndGetRedis(String str){
+        String redisKey = RedisKeyUtil.getRedisKey(str);
+        redisTemplate.opsForValue().set(redisKey,str);
+        return ApiResponse.ok(redisTemplate.opsForValue().get(redisKey));
     }
 }
---
+++ demo1/src/main/java/com/aco/practice/demo1/util/RedisKeyUtil.java
@@ -0,0 +1,11 @@
+package com.aco.practice.demo1.util;
+
+/**
+ * @author XHaoJian
+ * @data 2020/6/4 22:28
+ */
+public class RedisKeyUtil {
+    public static String getRedisKey(String str){
+        return "aco:" + str;
+    }
+}
