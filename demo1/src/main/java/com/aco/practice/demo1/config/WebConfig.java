22b71127684681e4f1d50c61dcdc2cd31ab187dd
Aco <519984084@qq.com>
2020-08-04 22:55:37 +0800

    [add]添加ajax请求，解决跨域问题

--- demo1/src/main/java/com/aco/practice/demo1/config/WebConfig.java
+++ demo1/src/main/java/com/aco/practice/demo1/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.aco.practice.demo1.handle.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -41,5 +42,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 }
---
+++ web/src/com.aco.practice.web/css/host.js
@@ -0,0 +1,2 @@
+let demo1Url = "http://127.0.0.1";
+let demo1Post = "8080";
